a5f56f5d03ecab648a69f2f3a85960634f4599ab
Evgenii <naterid@yandex.ru>
2023-03-27 23:12:54 +0700

    Добавляет вёрстку шапки

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import React from "react";
-import { useState } from "react";
-import Counter from "./components/Counter";
-import PostItem from "./components/PostItem";
-import PostList from "./components/PostList";
-import "./styles/style.css";
+
+import "./scss/app.scss";
+
+import geoIcon from "./assets/icons/header1.svg";
+import letterIcon from "./assets/icons/header2.svg";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: "1", title: "заголовок 1", description: "description 1" },
-    { id: "2", title: "заголовок 2", description: "description 2" },
-    { id: "3", title: "заголовок 3", description: "description 3" },
-  ]);
-
   return (
-    <div className="App">
-      <PostList posts={posts} title={"Список 1"} />
-    </div>
+    <>
+      <div className="wrapper">
+        <header className="header">
+          <div className="header__container">
+            <div className="header__top">
+              <div className="header__left-block left-block">
+                <div className="left-block__item ">
+                  <img className="left-block__image" src={geoIcon}></img>
+                  <div className="left-block__wrapper">
+                    <b>г. Кокчетав, ул. Ж. Ташенова 129Б </b>
+                    <p>(Рынок Восточный)</p>
+                  </div>
+                </div>
+                <div className="left-block__item">
+                  <img className="left-block__image" src={letterIcon}></img>
+                  <div className="left-block__wrapper">
+                    <a href="mailto:opt.sultan@mail.ru">opt.sultan@mail.ru</a>
+                    <p>На связи в любое время</p>
+                  </div>
+                </div>
+              </div>
+              <nav className="header__navigation navigation">
+                <ul className="navigation__body menu">
+                  <li className="menu__item">
+                    <a className="menu__link" href="">
+                      О компании
+                    </a>
+                  </li>
+                  <li className="menu__item">
+                    <a className="menu__link" href="">
+                      Доставка и оплата
+                    </a>
+                  </li>
+                  <li className="menu__item">
+                    <a className="menu__link" href="">
+                      Возврат
+                    </a>
+                  </li>
+                  <li className="menu__item">
+                    <a className="menu__link" href="">
+                      Контакты
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="header__bottom"></div>
+          </div>
+        </header>
+        <main className="main"></main>
+        <footer className="footer">asdasd</footer>
+      </div>
+    </>
   );
 }
 
