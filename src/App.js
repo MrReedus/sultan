61f5e30785cc04f5159564c59f42a571311ea970
Evgenii <naterid@yandex.ru>
2023-03-29 00:00:17 +0700

    добавляет сортировку и выносить её в отдельный компонент

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Sort from "./components/Sort";
 
 import "./scss/app.scss";
 
 function App() {
+  const [isVisible, setIsVisible] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+  const list = ["название", "цена"];
+
   return (
     <>
       <div className="wrapper">
         <Header />
 
-        <main className="main"></main>
-        <footer className="footer">asdasd</footer>
+        <main className="main">
+          <h1>Косметика и гигиена</h1>
+          <Sort />
+        </main>
+        <footer className="footer"></footer>
       </div>
     </>
   );
